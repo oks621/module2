b3339dbf3cee6ff858616b83158fcbb249644e9d
Oksana <briliant621621@gmail.com>
2021-11-21 20:11:01 +0200

    Aded exercise 7 class

---
+++ src/main/java/Exercise7.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class Exercise7 {
+    public static void main(String[] args) {
+        char englishA = 'A';
+        char anyRuLetter = 'ы';
+        char codeName70 = 'F';
+        System.out.println("englishA");
+        System.out.println("anyRuLetter");
+        System.out.println("codeName70");
+    }
+}
