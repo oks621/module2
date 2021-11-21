bc5634693a6e2f50f864c6faddaaa6c47ed72498
Oksana <briliant621621@gmail.com>
2021-11-21 17:03:15 +0200

    Aded exercise 1 class

---
+++ src/main/java/Exercise1.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+                byte happyValue =10;
+                System.out.println("happyValue");
+    }
+}
