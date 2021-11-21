a0ac80906f9f5b838d1842f9aaf234e32c6475fb
Oksana <briliant621621@gmail.com>
2021-11-21 20:39:46 +0200

    Aded exercise 8 class

---
+++ src/main/java/Exercise8.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class Exercise8 {
+        public static void main(String[] args) {
+            int x = 21;
+            boolean javaAfter20 = true;
+            boolean javaOpposite = false;
+            System.out.println("javaAfter20 = " + javaAfter20);
+            System.out.println("javaOpposite = " + javaOpposite);
+        }
+}
