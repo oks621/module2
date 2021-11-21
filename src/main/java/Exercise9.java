fb53c3c2d8101cf6f97c41665df98ea0027da5ff
Oksana <briliant621621@gmail.com>
2021-11-21 21:26:03 +0200

    Aded exercise 9 class

---
+++ src/main/java/Exercise9.java
@@ -0,0 +1,23 @@
+package main.java;
+
+public class Exercise9 {
+    public static void main(String[] args) {
+        byte allMonthCountButNotWinter = 9;
+        short february1996DayCount = 29;
+        int halfOfTotalSpringDayCount = 46;
+        long yearSecondCount = 31536000;
+        float quarterOfMayDayCount = 7.8f;
+        double dayCountOfLongMonths = 217d;
+        char springLetter = 'r';
+        boolean isLeap1900 = false;
+        System.out.println("allMonthCountButNotWinter");
+        System.out.println("february1996DayCount");
+        System.out.println("halfOfTotalSpringDayCount");
+        System.out.println("yearSecondCount");
+        System.out.println("quarterOfMayDayCount");
+        System.out.println("dayCountOfLongMonths");
+        System.out.println("springLetter");
+        System.out.println("isLeap1900");
+    }
+}
+
