2d817ab6f5a63751b7f4316dec2a03937f9c1ddf
Oksana <briliant621621@gmail.com>
2021-11-21 19:25:34 +0200

    Aded exercise 5 class

---
+++ src/main/java/Exercise5.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        float tooBigToFail = 1001f;
+        System.out.println("tooBigToFail");
+    }
+}
