a0f4bbad481bf3a2795324b6392a8e1cea0255e7
Oksana <briliant621621@gmail.com>
2021-11-21 19:42:38 +0200

    Aded exercise 6 class

---
+++ src/main/java/Exercise6.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+        double muchBiggerThanTooBig = -75d;
+        System.out.println("muchBiggerThanTooBig");
+    }
+}
