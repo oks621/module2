c91da5bc10534788793daaa9622dbd05eb1c94ed
Oksana <briliant621621@gmail.com>
2021-11-21 19:15:43 +0200

    Aded exercise 4 class

---
+++ src/main/java/Exercise4.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        long longerThanLife = 922337203685778L ;
+        System.out.println("longerThanLife");
+    }
+}
