314e19b6216ee4eb5ebcfbe3b2dc054cb2b9dfe6
Oksana <briliant621621@gmail.com>
2021-11-21 18:47:25 +0200

    Aded exercise 3 class

---
+++ src/main/java/Exercise3.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        int globalAnswer = 42;
+        int randomAnswer;
+        randomAnswer = 105;
+        System.out.println("globalAnswer");
+        System.out.println("randomAnswer");
+    }
+}
